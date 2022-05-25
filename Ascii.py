c5fd6744120c016cef646d5b883cd07cab1db3d5
LePhobix <kobalt6666@gmail.com>
2022-05-25 22:53:48 +0200

    Lines and Player movement!

--- Ascii.py
+++ Ascii.py
@@ -1,3 +1,4 @@
+from math import floor
 from numba import njit
 from numba.typed import List
 from keyboard import is_pressed
@@ -11,17 +12,20 @@ window_w = 0
 window_h = 0
 window_border_char = ''
 window_name = ''
+window_manual_update = False
+window_invoke_manual_update = False
 fps = 0
 
 
 # Init window
-def init(width, height, border_char, frame_rate, name='Default'):
-    global window_w, window_h, window_border_char, window_name, window_initialized, fps
+def init(width, height, border_char, frame_rate, manual_update=False, name='Default'):
+    global window_w, window_h, window_border_char, window_name, window_initialized, window_manual_update, fps
 
     window_w = width
     window_h = height
     window_border_char = border_char
     window_name = name
+    window_manual_update = manual_update
     fps = frame_rate
     window_initialized = True
 
@@ -52,6 +56,31 @@ def draw_rect_on_top(x, y, w, h, array, char='#'):
 
 @njit
 def draw_line_on_top(x1, y1, x2, y2, array, char='#'):
+    dx = x2 - x1
+    dy = y2 - y1
+    
+    steps = 0
+    if abs(dx) > abs(dy):
+        steps = abs(dx)
+    else:
+        steps = abs(dy)
+    
+    if steps == 0:
+        xinc = 0
+        yinc = 0
+    else:
+        xinc = dx / steps
+        yinc = dy / steps
+    
+    
+    for i in range(steps):
+        x = floor(x1 + xinc * i) 
+        y = floor(y1 + yinc * i)
+        
+        idx = x + y * window_w
+        if idx < len(array) and idx > -1 and x < window_w and x > -1:
+            array[idx] = char
+    
     return array
 
 
@@ -79,49 +108,55 @@ def draw(array):
 
 
 def check_input():
-    global run_AsciiLib
+    global run_AsciiLib, window_invoke_manual_update, y, x
     if is_pressed('esc'):
         print("Shutting down AsciiLib.py")
         run_AsciiLib = False
+    if is_pressed('+'):
+        window_invoke_manual_update = True
+    
+    # Player Input
+    if is_pressed('up'):
+        y -= 1
+    if is_pressed('down'):
+        y += 1
+    if is_pressed('left'):
+        x -= 1
+    if is_pressed('right'):
+        x += 1
 
 ###########################################################################################################################################################################
 
 
-init(25, 25, '.', .1, 'Test Window')
+init(75, 25, '.', .01)
 screen_buffer_array = init_screen_buffer()
 system('CLS')
 
-
-def one_time():
-    # Drawing stuff
-    screen_buffer_array = init_screen_buffer()
-    screen_buffer_array = draw_rect_on_top(10, 10, 5, 5, screen_buffer_array)
-    screen_buffer_array = draw_line_on_top(20, 20, 30, 30, screen_buffer_array)
-    screen_buffer = draw(screen_buffer_array)
-
-    system('CLS')
-    print(screen_buffer)
-    exit(0)
-
-x = -20
-y = -20
+x = 10
+y = 10
 while run_AsciiLib:
+    if window_manual_update:
+        check_input()
+        sleep(.1)
+        
+        if window_invoke_manual_update:
+            window_invoke_manual_update = False
+        else:
+            continue
+    
     # Drawing stuff
     screen_buffer_array = init_screen_buffer()
-    screen_buffer_array = draw_rect_on_top(x, y, 15, 5, screen_buffer_array)
-    screen_buffer_array = draw_rect_on_top(-x, -y, 5, 5, screen_buffer_array, '@')
-    screen_buffer_array = draw_line_on_top(20, 20, 30, 30, screen_buffer_array, '@')
+    screen_buffer_array = draw_rect_on_top(x, y, 2, 2, screen_buffer_array, '@')
+    screen_buffer_array = draw_line_on_top(20, 20, x, y, screen_buffer_array, 'x')
     screen_buffer = draw(screen_buffer_array)
 
     system('CLS')
     print(screen_buffer)
-
-    x += 1
-    y += 1
 
     # Other stuff
-    sleep(fps)
-    check_input()
+    if not window_manual_update:
+        sleep(fps)
+        check_input()
 
 system('CLS')
 print("Thanks for using AsciiLib :)")
